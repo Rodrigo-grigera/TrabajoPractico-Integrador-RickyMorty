bb82de8d1b936b35f6c3c32647d3d1965eae6577
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-08-27 13:21:15 -0300

    Refactoriando el proyecto, arreglado completo

--- index.js
+++ index.js
@@ -1,134 +1,66 @@
 const API = "https://rickandmortyapi.com/api/character";
 
 let paginaActual = 1;
-let estado = "";
+let state = "";
 let nombre = "";
 
 const getPersonajes = async () => {
   try {
-    const respo = await fetch(
-      `${API}?page=${paginaActual}&name=${nombre}&status=${estado}`,
+    const resp = await fetch(
+      `${API}/?page=${paginaActual}&name=${nombre}&status=${state}`,
     );
-    if (respo.ok) {
-      const personaje = await respo.json();
-      const pagiTotales = personaje.info.pages;
-
-      sigueinte.disabled = paginaActual === pagiTotales;
-      anterior.disabled = paginaActual === 1;
-
-      let ids = [];
-      for (let i = 0; i < personaje.results.length; i++) {
-        ids.push(personaje.results[i].id);
-      }
-      documento.innerHTML = "";
-
-      for (let i = 0; i < ids.length; i++) {
-        const id = ids[i];
-        await crearCarta(id);
-      }
-    } else {
-      sigueinte === true;
-      anterior === true;
+    if (!resp.ok) {
+      throw new Error("Error de al cargar los personajes");
     }
+    const data = await resp.json();
+    return data.results;
   } catch (error) {
-    return error.message;
+    throw error;
   }
 };
-
-getPersonajes();
-
-const getPersona = async (id) => {
-  try {
-    const respo = await fetch(`${API}/${id}`);
-    if (respo.ok) {
-      const persona = await respo.json();
-      // console.log(persona);
-      return persona;
-    }
-  } catch (error) {
-    console.log(error.status);
-  }
-};
-// personaje("1");
 
 const documento = document.getElementById("tarjeta");
 
-const modal = document.getElementById("modal");
-const modalBody = document.getElementById("modal-body");
-const cerrar = document.getElementById("cerrar");
+const crearCarta = async () => {
+  try {
+    const pers = await getPersonajes();
+    pers.forEach((element) => {
+      const carta = document.createElement("div"); //creamos un elemneto de forma directa
+      carta.classList.add("carta");
+      carta.innerHTML = `
+          <img src="${element.image}" alt="${element.name}">
+          <h2>${element.name}</h2>`;
+      documento.appendChild(carta);
+    });
 
-cerrar.onclick = () => {
-  modal.close();
-};
-
-const crearCarta = async (id) => {
-  const infoPerso = await getPersona(id);
-
-  const carta = document.createElement("div"); //creamos un elemneto de forma directa
-  carta.classList.add("carta");
-  carta.style.width = "18rem";
-
-  carta.innerHTML = `
-    <img src="${infoPerso.image}" alt="${infoPerso.name}">
-    <h2>${infoPerso.name}</h2>
-    `;
-  carta.addEventListener("click", () => {
-    modalBody.innerHTML = `
-            <h2>${infoPerso.name}</h2>
-            <img src="${infoPerso.image}" alt="${infoPerso.name}" style="width:80%" />
-            <p><span>Estado:</span> ${infoPerso.status}</p>
-            <p><span>Género:</span> ${infoPerso.gender}</p>
-            <p><span>Especie:</span> ${infoPerso.species}</p>
-            <p><span>Origen:</span> ${infoPerso.origin.name}</p>
-            <p><span>Ubicación actual:</span> ${infoPerso.location.name}</p>
-          `;
-    modal.showModal();
-  });
-
-  documento.appendChild(carta);
-};
-
-const sigueinte = document.getElementById("siguiente");
-const anterior = document.getElementById("anterior");
-
-function pagAnterior() {
-  paginaActual--;
-  getPersonajes();
-}
-anterior.addEventListener("click", pagAnterior);
-
-function pagSiguiente() {
-  paginaActual++;
-  getPersonajes();
-}
-sigueinte.addEventListener("click", pagSiguiente);
-
-const filtroEstado = document.getElementById("filtroEstado");
-const boton = document.getElementById("boton");
-
-function filtrar(e) {
-  e.preventDefault();
-  estado = filtroEstado.value;
-  paginaActual = 1;
-  getPersonajes();
-}
-boton.addEventListener("click", filtrar);
-
-const input = document.getElementById("input");
-const buscar = document.getElementById("buscar");
-
-function buscarPorNombre(e) {
-  e.preventDefault();
-  nombre = input.value;
-  input.value = "";
-
-  if (input != nombre) {
-    sigueinte.disabled = true;
-    documento.innerHTML = `
-        <h3>El Personaje no existe</h3>`;
+  } catch (error) {
+    const mensaje = document.createElement("p");
+    mensaje.innerHTML = "No se pudieron cargar los personajes";
+    documento.appendChild(mensaje);
   }
-  paginaActual = 1;
-  getPersonajes();
-}
+};
 
-buscar.addEventListener("click", buscarPorNombre);
+crearCarta();
+
+const btn_next = document.getElementById("next");
+const btn_back = document.getElementById("back");
+
+const paginaSiguiente = () => {
+  paginaActual++;
+  if (paginaActual > 1) btn_back.disabled = false;
+  if (paginaActual === 42) btn_next.disabled = true;
+  documento.innerHTML = "";
+  crearCarta();
+};
+
+btn_next.addEventListener("click", paginaSiguiente);
+
+const pagAnterior = () => {
+  paginaActual = paginaActual - 1;
+  if (paginaActual === 1) {
+    btn_back.disabled = true;
+  }
+  documento.innerHTML = "";
+  crearCarta();
+};
+btn_back.addEventListener("click", pagAnterior);
